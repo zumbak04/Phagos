8c86a86291d8be95dd785f3d7ea4e530af831a45
zumbak04 <46384992+zumbak04@users.noreply.github.com>
2021-09-01 00:28:00 +0400

    Moving Energy Loss Per Acceleration It's too hard to balance so I want to move it out for now.

--- Assets/Assets/Scripts/Bacterium.cs
+++ Assets/Assets/Scripts/Bacterium.cs
@@ -167,7 +167,7 @@ public class Bacterium : MonoBehaviour
         velocity *= 0.95f;
         rigidBody.velocity = velocity;
 
-        SpendEnergy(acceleration);
+        SpendEnergy();
     }
 
     public void Die()
@@ -269,10 +269,10 @@ public class Bacterium : MonoBehaviour
         Bacterium childBacterium = GameManager.instance.SpawnBacterium(rigidBody.transform.position,childGenome);
         childBacterium.energy = energy;
     }
-    public void SpendEnergy(Vector2 velocityChange)
+    public void SpendEnergy()
     {
         //Spends energy
-        energyCost = Time.deltaTime * (GameManager.instance.energyPerSecondLoss + velocityChange.magnitude * rigidBody.mass * GameManager.instance.energyPerSizeLoss);
+        energyCost = Time.deltaTime * (GameManager.instance.energyPerSecondLoss + rigidBody.mass * GameManager.instance.energyPerSizeLoss);
         energy -= energyCost;
 
         //If energy is low, dies
